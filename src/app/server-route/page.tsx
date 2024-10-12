72d681379930378e933aab202b4ea82438a79b62
Mark <metalnight24@hotmail.com>
2024-10-12 10:57:01 -0400

    Third Party Packages

--- src/app/server-route/page.tsx
+++ src/app/server-route/page.tsx
@@ -1,12 +1,14 @@
+import { ImageSlider } from "@/components/ImageSlider";
 import { serverSideFunction } from "@/utils/server-utils";
 
 export default function ServerRoutePage() {
-	console.log("Server Route Rendered");
+	console.log("Server Route Rendereds");
 	const result = serverSideFunction();
 	return (
 		<>
 			<h1>ServerRoutePage</h1>
 			<p>{result}</p>
+			<ImageSlider />
 		</>
 	);
 }
--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -4,7 +4,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-export default function ClientRoutePage() {
+export const ImageSlider = () => {
 	const settings = {
 		dots: true,
 	};
@@ -26,4 +26,4 @@ export default function ClientRoutePage() {
 			</Slider>
 		</div>
 	);
-}
+};
