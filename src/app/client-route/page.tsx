60e5f815a185403b43cedd7d70e5dd5c8bac69e2
Mark <metalnight24@hotmail.com>
2024-10-12 11:10:27 -0400

    Context Providers

--- src/app/client-route/page.tsx
+++ src/app/client-route/page.tsx
@@ -3,27 +3,32 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { useTheme } from "@/components/theme-provider";
 
 export default function ClientRoutePage() {
+	const theme = useTheme();
 	const settings = {
 		dots: true,
 	};
 	return (
-		<div className="image-slider-container">
-			<Slider {...settings}>
-				<div>
-					<img src="http://picsum.photos/400/200" />
-				</div>
-				<div>
-					<img src="http://picsum.photos/400/200" />
-				</div>
-				<div>
-					<img src="http://picsum.photos/400/200" />
-				</div>
-				<div>
-					<img src="http://picsum.photos/400/200" />
-				</div>
-			</Slider>
-		</div>
+		<>
+			<h1 style={{ color: theme.colors.primary }}>ClientRoute</h1>
+			<div className="image-slider-container">
+				<Slider {...settings}>
+					<div>
+						<img src="http://picsum.photos/400/200" />
+					</div>
+					<div>
+						<img src="http://picsum.photos/400/200" />
+					</div>
+					<div>
+						<img src="http://picsum.photos/400/200" />
+					</div>
+					<div>
+						<img src="http://picsum.photos/400/200" />
+					</div>
+				</Slider>
+			</div>
+		</>
 	);
 }
